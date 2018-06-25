6f28ff7cd3f194ce96a3c3bf9814e8887b63a28f
Elliando Dias Carneiro Santos <elliando.santos@PMCG.IMTI>
2018-06-25 15:35:41 -0400

    Inserindo um novo Cliente com POST

--- src/main/java/com/nelioalves/cursomc/domain/Endereco.java
+++ src/main/java/com/nelioalves/cursomc/domain/Endereco.java
@@ -23,7 +23,7 @@ public class Endereco implements Serializable {
 	private String numero;
 	private String complemento;
 	private String bairro;
-	private String cpf;
+	private String cep;
 	
 	@JsonIgnore
 	@ManyToOne
@@ -37,7 +37,7 @@ public class Endereco implements Serializable {
 	public Endereco() {
 	}
 
-	public Endereco(Integer id, String logradouro, String numero, String complemento, String bairro, String cpf,
+	public Endereco(Integer id, String logradouro, String numero, String complemento, String bairro, String cep,
 			Cliente cliente, Cidade cidade) {
 		super();
 		this.id = id;
@@ -45,7 +45,7 @@ public class Endereco implements Serializable {
 		this.numero = numero;
 		this.complemento = complemento;
 		this.bairro = bairro;
-		this.cpf = cpf;
+		this.cep = cep;
 		this.cliente = cliente;
 		this.cidade = cidade;
 	}
@@ -90,12 +90,12 @@ public class Endereco implements Serializable {
 		this.bairro = bairro;
 	}
 
-	public String getCpf() {
-		return cpf;
+	public String getCep() {
+		return cep;
 	}
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
+	public void setCep(String cep) {
+		this.cep = cep;
 	}
 
 	public Cidade getCidade() {
